d81d2f20c39430e40b0e86a353d7546064038c6d
MeldoyZhang <1781455024@qq.com>
2022-03-24 15:48:20 +0800

    优化注册页面逻辑

--- app.js
+++ app.js
@@ -10,7 +10,11 @@ App({
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
-        // "pages/registrationProblem/registrationProblem",
+        if (!res.code) {
+          wx.navigateTo({
+            url: "/pages/registrationProblem/registrationProblem",
+          })
+        }
       }
     })
     // 获取系统信息
